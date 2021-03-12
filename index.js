e0f3680d2d0aa716db56581941b143775969d9aa
budigk <budigk@gmail.com>
2021-03-12 22:02:16 +0800

    perapian script

--- index.js
+++ index.js
@@ -1,3 +1,117 @@
+let menus = [
+  {
+    id: '001',
+    nama: 'Nasi Goreng Setan',
+    harga: '45000',
+    image: 'images/nasi.jpg',
+  },
+  {
+    id: '002',
+    nama: 'Mie Goreng Ala Mamak',
+    harga: '42000',
+    image: 'images/mie.jpg'
+  },
+  {
+    id: '003',
+    nama: 'Kwetiau Goreng Antri',
+    harga: '42000',
+    image: 'images/kwetiau.jpg'
+  },
+  {
+    id: '004',
+    nama: 'Bihun Goreng Hongkong',
+    harga: '40000',
+    image: 'images/bihun.jpg'
+  },
+  {
+    id: '005',
+    nama: 'Sate Ayam Lecker',
+    harga: '25000',
+    image: 'images/sate.jpg'
+  },
+  {
+    id: '006',
+    nama: 'Spicy French Fries',
+    harga: '12000',
+    image: 'images/kentang.jpg'
+  },
+  {
+    id: '007',
+    nama: 'Spaghetti Poll Pedas',
+    harga: '55000',
+    image: 'images/spaghetti.jpg'
+  },
+  {
+    id: '008',
+    nama: 'Pizza Pepperoni',
+    harga: '85000',
+    image: 'images/pizza.jpg'
+  },
+  {
+    id: '009',
+    nama: 'Es Degan Nikmat',
+    harga: '25000',
+    image: 'images/degan.jpg'
+  },
+  {
+    id: '010',
+    nama: 'Es Mojito',
+    harga: '35000',
+    image: 'images/mojito.jpg'
+  },
+  {
+    id: '011',
+    nama: 'Kopi Gula Aren',
+    harga: '25000',
+    image: 'images/kopi.jpg'
+  },
+  {
+    id: '012',
+    nama: 'Teh Pilihan',
+    harga: '25000',
+    image: 'images/teh.jpg'
+  },
+];
+
+function generateTableMenu(table, data) {
+  let iCol = 1;
+  let row;
+  for (let element of data) {
+      if (iCol == 1){
+        row = table.insertRow();
+      }
+
+      let cell = row.insertCell();
+
+      const image = document.createElement('img')
+      image.src  = element.image
+      image.alt = "Foto Menu Makanan"
+      image.width = 250
+      image.id = element.id
+      cell.id = element.id
+      cell.appendChild(image)
+
+      let nf = new Intl.NumberFormat();
+
+      let text = document.createTextNode(element.nama+ " [ " + nf.format(element.harga) + " ]");
+      text.id = element.id;
+      cell.appendChild(text);
+
+      iCol += 1;
+
+      if (iCol == 5){
+        iCol = 1;
+      }
+  }
+
+  for (var i = 0; i < table.rows.length; i++) {
+      for (var j = 0; j < table.rows[i].cells.length; j++)
+      table.rows[i].cells[j].onclick = function () {
+          menuClick(this);
+      };
+  }
+}
+
 function generateTablePesanan(table, data) {
     //Remove All Rows
     
@@ -69,45 +183,6 @@ function generateTablePesanan(table, data) {
     cellTotal.appendChild(totalNode);  
 }
 
-function generateTableMenu(table, data) {
-  let iCol = 1;
-  let row;
-  for (let element of data) {
-      if (iCol == 1){
-        row = table.insertRow();
-      }
-
-      let cell = row.insertCell();
-
-      const image = document.createElement('img')
-      image.src  = element.image
-      image.alt = "Foto Menu Makanan"
-      image.width = 250
-      image.id = element.id
-      cell.id = element.id
-      cell.appendChild(image)
-
-      let nf = new Intl.NumberFormat();
-
-      let text = document.createTextNode(element.nama+ " [ " + nf.format(element.harga) + " ]");
-      text.id = element.id;
-      cell.appendChild(text);
-
-      iCol += 1;
-
-      if (iCol == 5){
-        iCol = 1;
-      }
-  }
-
-  for (var i = 0; i < table.rows.length; i++) {
-      for (var j = 0; j < table.rows[i].cells.length; j++)
-      table.rows[i].cells[j].onclick = function () {
-          menuClick(this);
-      };
-  }
-}
-
 function menuClick(tableCell) {
   alert("Pesanan Anda Telah Tersimpan")
   objMenu = arrayToObject(menus);
@@ -133,81 +208,5 @@ function arrayToObject(array){
 }
 
 let pesanan = {};
-
-let menus = [
-    {
-      id: '001',
-      nama: 'Nasi Goreng Setan',
-      harga: '45000',
-      image: 'images/nasi.jpg',
-    },
-    {
-      id: '002',
-      nama: 'Mie Goreng Ala Mamak',
-      harga: '42000',
-      image: 'images/mie.jpg'
-    },
-    {
-      id: '003',
-      nama: 'Kwetiau Goreng Antri',
-      harga: '42000',
-      image: 'images/kwetiau.jpg'
-    },
-    {
-      id: '004',
-      nama: 'Bihun Goreng Hongkong',
-      harga: '40000',
-      image: 'images/bihun.jpg'
-    },
-    {
-      id: '005',
-      nama: 'Sate Ayam Lecker',
-      harga: '25000',
-      image: 'images/sate.jpg'
-    },
-    {
-      id: '006',
-      nama: 'Spicy French Fries',
-      harga: '12000',
-      image: 'images/kentang.jpg'
-    },
-    {
-      id: '007',
-      nama: 'Spaghetti Poll Pedas',
-      harga: '55000',
-      image: 'images/spaghetti.jpg'
-    },
-    {
-      id: '008',
-      nama: 'Pizza Pepperoni',
-      harga: '85000',
-      image: 'images/pizza.jpg'
-    },
-    {
-      id: '009',
-      nama: 'Es Degan Nikmat',
-      harga: '25000',
-      image: 'images/degan.jpg'
-    },
-    {
-      id: '010',
-      nama: 'Es Mojito',
-      harga: '35000',
-      image: 'images/mojito.jpg'
-    },
-    {
-      id: '011',
-      nama: 'Kopi Gula Aren',
-      harga: '25000',
-      image: 'images/kopi.jpg'
-    },
-    {
-      id: '012',
-      nama: 'Teh Pilihan',
-      harga: '25000',
-      image: 'images/teh.jpg'
-    },
-  ];
-
 let tabelMenu = document.getElementById("tabelMenu")
 generateTableMenu(tabelMenu, menus)
